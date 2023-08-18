e526b45e7822be0f1af7d5ec9139405a33185658
Yurii Karabas <1998uriyyo@gmail.com>
2023-08-18 16:04:07 +0300

    Export sorting API

--- fastapi_filters/__init__.py
+++ fastapi_filters/__init__.py
@@ -1,13 +1,18 @@
 from .fields import FieldFilter
 from .filters import create_filters, create_filters_from_model
 from .operators import FilterOperator
-from .types import FilterValues, FiltersResolver
+from .types import FilterValues, FiltersResolver, SortingResolver, SortingValues
+from .sorters import create_sorting, create_sorting_from_model
 
 __all__ = [
+    "SortingValues",
+    "SortingResolver",
     "FilterValues",
     "FiltersResolver",
     "FieldFilter",
     "FilterOperator",
     "create_filters",
     "create_filters_from_model",
+    "create_sorting",
+    "create_sorting_from_model",
 ]
