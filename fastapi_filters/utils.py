c6b1b54a2af887eb70f8a41b98d206d9e7ddbf57
Yurii Karabas <1998uriyyo@gmail.com>
2025-10-14 14:28:16 +0300

    Update utils.py

--- fastapi_filters/utils.py
+++ fastapi_filters/utils.py
@@ -7,11 +7,16 @@ from typing import (
     Union,
 )
 
-from fastapi._compat import field_annotation_is_complex
 from pydantic.fields import FieldInfo
 from pydantic.v1.typing import get_args, get_origin, is_none_type, is_union
 from pydantic.v1.utils import lenient_issubclass
 from typing_extensions import ParamSpec
+
+
+try:
+    from fastapi._compat import field_annotation_is_complex
+except ImportError:
+    from fastapi._compat.shared import field_annotation_is_complex
 
 P = ParamSpec("P")
 T = TypeVar("T")
