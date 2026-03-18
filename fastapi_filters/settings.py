a37dc5d25089ba97742e2f9cd275e37e2ba897cd
Ivo Donchev <ivaylo.donchev@waracle.com>
2026-03-18 13:16:27 +0200

    Add .env configuration

--- fastapi_filters/settings.py
+++ fastapi_filters/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class FastAPIFiltersSettings(BaseSettings):
     csv_separator: str = ","
-    model_config = SettingsConfigDict(env_prefix="FASTAPI_FILTERS_")
+    model_config = SettingsConfigDict(env_prefix="FASTAPI_FILTERS_", env_file=".env")
 
 
 app_settings = FastAPIFiltersSettings()
