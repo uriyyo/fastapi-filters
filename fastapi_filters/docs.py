cfeefbaad772b837895d720370a15a0fd49ebc6e
Yurii Karabas <1998uriyyo@gmail.com>
2026-05-24 21:28:34 +0200

    Remove usage of deprecated FastAPI startup events (#731)

--- fastapi_filters/docs.py
+++ fastapi_filters/docs.py
@@ -1,17 +1,38 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+from functools import partial
+from typing import Any
+
 from fastapi import FastAPI
+from starlette.types import Lifespan
+
+
+def _fix_docs(app: FastAPI) -> None:
+    openapi = app.openapi()
+
+    for endpoints in openapi["paths"].values():
+        for endpoint in endpoints.values():
+            for parameter in endpoint.get("parameters", ()):
+                if "explode" in parameter["schema"]:
+                    parameter["explode"] = parameter["schema"].pop("explode")
+
+
+@asynccontextmanager
+async def _lifespan_wrapper(
+    _lifespan_context: Lifespan[Any],
+    app: FastAPI,
+) -> AsyncGenerator[Any]:
+    _fix_docs(app)
+
+    async with _lifespan_context(app) as state:
+        yield state
 
 
 def fix_docs(app: FastAPI) -> None:
-    def _fix_docs() -> None:
-        openapi = app.openapi()
-
-        for endpoints in openapi["paths"].values():
-            for endpoint in endpoints.values():
-                for parameter in endpoint.get("parameters", ()):
-                    if "explode" in parameter["schema"]:
-                        parameter["explode"] = parameter["schema"].pop("explode")
-
-    app.add_event_handler("startup", _fix_docs)
+    app.router.lifespan_context = partial(
+        _lifespan_wrapper,
+        app.router.lifespan_context,
+    )
 
 
 __all__ = [
